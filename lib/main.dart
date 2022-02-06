7970966eae08b9fd574dba6826b292f06c9285f2
aprxty3 <ajiprxty3@gmail.com>
2022-02-06 22:52:36 +0700

    build main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shoe_apps/ui/pages/sign_in_page.dart';
+import 'package:shoe_apps/ui/pages/sign_up_page.dart';
+import 'package:shoe_apps/ui/pages/splash_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,17 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => SplashPage(),
+        '/sign_in': (context) => SignInPage(),
+        '/sign_up': (context) => SignUpPage(),
+      },
     );
   }
 }
