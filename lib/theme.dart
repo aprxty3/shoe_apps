0ea79858146b5dc675e1d00bd49145e719e65783
aprxrt3 <ajiprxty3@gmail.com>
2022-02-07 00:21:13 +0700

    add some theme style

--- lib/theme.dart
+++ lib/theme.dart
@@ -8,43 +8,49 @@ Color priceColor = const Color(0xff2C96F1);
 Color kBg1 = const Color(0xff1F1D2B);
 Color kBg2 = const Color(0xff2B2937);
 Color kBg3 = const Color(0xff242231);
+Color kBg4 = const Color(0xff504F5E);
 Color primaryTextColor = const Color(0xffF1F0F2);
 Color secondaryTextColor = const Color(0xff999999);
 
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
 //SIGN IN/UP
 
-// TextStyle mainText = GoogleFonts.poppins(
-//   fontSize: 24,
-//   fontWeight: FontWeight.w600,
-//   color: primaryTextColor,
-// );
+TextStyle mainText = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+  color: primaryTextColor,
+);
 
-// TextStyle secondText = GoogleFonts.poppins(
-//   fontSize: 14,
-//   fontWeight: FontWeight.w400,
-//   color: Color(0xff504F5E),
-// );
+TextStyle secondText = const TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  color: Color(0xff504F5E),
+);
 
-// TextStyle suggestStyle = GoogleFonts.poppins(
-//   fontSize: 16,
-//   fontWeight: FontWeight.w500,
-//   color: primaryTextColor,
-// );
+TextStyle suggestStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+  color: primaryTextColor,
+);
 
-// TextStyle hintStyle = GoogleFonts.poppins(
-//   fontSize: 14,
-//   fontWeight: FontWeight.w400,
-//   color: const Color(0xff504F5E),
-// );
+TextStyle hintStyle = const TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  color: Color(0xff504F5E),
+);
 
-// TextStyle buttonStyle = GoogleFonts.poppins(
-//   fontSize: 16,
-//   fontWeight: FontWeight.w500,
-//   color: primaryTextColor,
-// );
+TextStyle buttonStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+  color: primaryTextColor,
+);
 
-// TextStyle alertStyle = GoogleFonts.poppins(
-//   fontSize: 10,
-//   fontWeight: FontWeight.w300,
-//   color: alertColor,
-// );
+TextStyle alertStyle = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w300,
+  color: alertColor,
+);
