e624d486f8014caae2820bf71b1ffcc6da052da1
aprxrt3 <ajiprxty3@gmail.com>
2022-02-07 00:25:26 +0700

    add header

--- lib/ui/pages/sign_up_page.dart
+++ lib/ui/pages/sign_up_page.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
 
+import '../../theme.dart';
+
 class SignUpPage extends StatelessWidget {
   const SignUpPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    Widget header() {
+      return Container(
+        margin: const EdgeInsets.only(top: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Sign Up',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
+                color: primaryTextColor,
+              ),
+            ),
+            const SizedBox(
+              height: 2,
+            ),
+            Text(
+              'Register and Happy Shoping',
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+                color: secondaryTextColor,
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: kBg1,
+      body: ListView(
+        padding: const EdgeInsets.all(30),
+        children: [
+          header(),
+        ],
+      ),
+    );
   }
 }
