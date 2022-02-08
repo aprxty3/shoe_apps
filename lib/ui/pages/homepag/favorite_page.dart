35d64154084f4ade20f30d48a346d9e2524d211a
aprxty3 <ajiprxty3@gmail.com>
2022-02-09 03:43:55 +0700

    build page

---
+++ lib/ui/pages/homepag/favorite_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../../../theme.dart';
+
+class FavPage extends StatelessWidget {
+  const FavPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: kBg1,);
+  }
+}
